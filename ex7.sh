463d504ad3d885f0af7e495f3ac6e1279ee48671
harithasreematcha <114555481+harithasreematcha@users.noreply.github.com>
2022-09-28 11:09:54 +0530

    Add files via upload

---
+++ ex7.sh
@@ -0,0 +1,13 @@
+#to print multiplication table of given number
+echo "enter  value:"
+read n
+i=1
+while [ $i -le 10 ]
+do
+#2*1=2
+#2*2=4
+res=`expr $n \* $i`
+echo "$n*$i=$res"
+i=`expr $i + 1`
+done
+
